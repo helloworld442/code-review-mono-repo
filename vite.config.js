e6100bf136f4ac35548b1d867190680babdd05b9
김민승 <min@gimminseung-ui-MacBookAir.local>
2023-08-23 06:22:35 +0900

    feat : feat text zip performace optimizing #20

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
+import { compression } from "vite-plugin-compression2";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr()],
+  plugins: [
+    react(),
+    svgr(),
+    compression({
+      include: [/\.(js)$/, /\.(css)%/],
+      threshold: 1400,
+    }),
+  ],
 });
