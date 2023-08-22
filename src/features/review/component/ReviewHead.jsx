c762b47867980a33234aa7769ba5344baddf735f
김민승 <min@gimminseung-ui-MacBookAir.local>
2023-08-22 13:25:43 +0900

    fix : fix review header and ui header layout #14

--- src/features/review/component/ReviewHead.jsx
+++ src/features/review/component/ReviewHead.jsx
@@ -4,6 +4,7 @@ import { Tag } from "../../ui";
 const ReviewHead = () => {
   return (
     <div className="review-head">
+      <h2 className="review-head-title">기술 스택</h2>
       <Tag title="JavaScript" color="yellow" size="large" />
       <Tag title="NodeJs" color="green" size="large" />
       <Tag title="ReactJs" color="blue" size="large" />
