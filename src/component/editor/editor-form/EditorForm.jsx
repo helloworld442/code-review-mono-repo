f744e9b5b55e18b546047eda433762c38f6d8797
김민승 <min@gimminseung-ui-MacBookAir.local>
2023-08-26 23:59:04 +0900

    fix : fix review editor logic ver 4 #75

--- src/component/editor/editor-form/EditorForm.jsx
+++ src/component/editor/editor-form/EditorForm.jsx
@@ -32,25 +32,19 @@ const EditorForm = ({ name, onCode }) => {
         'function $1(<span class = "hg-fc-parenthesis-code">$2</span>)'
       )
       .replace(
-        /(for|while)(\([^)]*\))/g,
-        '<span class = "hg-block-code">$1<span class = "hg-block-parenesis-code">$2</span></span>'
-      )
-      .replace(
-        /(if|else|else if)(\([^)]*\))/g,
-        '<span class = "hg-conditional-code">$1<span class = "hg-con-parenesis-code">$2</span></span>'
-      )
-      .replace(
         /(\w+)(\([^)]*\))/g,
         '<span class = "hg-word-code">$1<span class = "hg-word-parenthesis-code">$2</span></span>'
       )
-      .replace(/(import|export|from|default)/g, '<span class = "hg-gate-code">$1</span>')
-      .replace(/(function)/g, '<span class = "hg-function-keyword-code">$1</span>')
-      .replace(/(const|var|let)/g, '<span class = "hg-var-code">$1</span>')
-      .replace(/(break|continue)/g, '<span class = "hg-block-return-code">$1</span>')
-      .replace(/(return)/g, '<span class = "hg-return-code">$1</span>')
-      .replace(/(null|undefined|false|true)/g, '<span class = "hg-null-code">$1</span>')
-      .replace(/(className)/g, '<span class = "hg-classnames-code">$1</span>')
-      .replace(/(\d)/g, '<span class = "hg-num-code">$1</span>')
+      .replace(/\b(for|while)\b/g, '<span class="hg-block-code">$1</span>')
+      .replace(/\b(if|else|else if)\b/g, '<span class="hg-conditional-code">$1</span>')
+      .replace(/\b(import|export|from|default)\b/g, '<span class = "hg-gate-code">$1</span>')
+      .replace(/\b(function)\b/g, '<span class = "hg-function-keyword-code">$1</span>')
+      .replace(/\b(const|var|let)\b/g, '<span class = "hg-var-code">$1</span>')
+      .replace(/\b(break|continue)\b/g, '<span class = "hg-block-return-code">$1</span>')
+      .replace(/\b(return)\b/g, '<span class = "hg-return-code">$1</span>')
+      .replace(/\b(null|undefined|false|true)\b/g, '<span class = "hg-null-code">$1</span>')
+      .replace(/\b(className)\b/g, '<span class = "hg-classnames-code">$1</span>')
+      .replace(/\b(\d)\b/g, '<span class = "hg-num-code">$1</span>')
       .replace(/\/\/.*/g, '<span class = "hg-comment-code">$&</span>')
       .replace(/\[([^\[\]=<>]*)\]/g, '<span class = "hg-bracket-var-code">[$1]</span>')
       .replace(/(\(|\))/g, '<span class = "hg-parenthesis-code">$1</span>')
